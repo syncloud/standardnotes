b2b09f3ebc9c952c98c4a2202004ced766de3045
Boris Rybalkin <ribalkin@gmail.com>
2019-02-28 08:47:07 +0000

    fix device

--- integration/verify.py
+++ integration/verify.py
@@ -25,8 +25,8 @@ TMP_DIR = '/tmp/syncloud'
 
 
 @pytest.fixture(scope="session")
-def module_setup(request, device_host, data_dir, platform_data_dir, app_dir, log_dir, app):
-    request.addfinalizer(lambda: module_teardown(device_host, data_dir, platform_data_dir, app_dir, log_dir, app))
+def module_setup(request, device, data_dir, platform_data_dir, app_dir, log_dir, app):
+    request.addfinalizer(lambda: module_teardown(device, data_dir, platform_data_dir, app_dir, log_dir, app))
 
 
 def module_teardown(device, data_dir, platform_data_dir, app_dir, log_dir, app):
