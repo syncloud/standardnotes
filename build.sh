5c3774ee437b920f30616b88b062bea5c333a814
Boris Rybalkin <ribalkin@gmail.com>
2019-02-28 07:28:06 +0000

    download standard file build

--- build.sh
+++ build.sh
@@ -12,9 +12,12 @@ export TMPDIR=/tmp
 export TMP=/tmp
 
 NAME=$1
-WORDPRESS_VERSION=5.0.3
-WORDPRESS_LDAP_VERSION=3.0.2
+STANDARD_FILES_VERSION=0.3.3
 ARCH=$(uname -m)
+STANDARD_FILES_ARCH=64-bit
+if [[ ${ARCH} == "arm" ]]; then
+    STANDARD_FILES_ARCH=arm6
+fi
 SNAP_ARCH=$(dpkg --print-architecture)
 VERSION=$2
 
@@ -23,56 +26,24 @@ DOWNLOAD_URL=http://artifact.syncloud.org/3rdparty
 rm -rf ${DIR}/build
 BUILD_DIR=${DIR}/build/${NAME}
 mkdir -p ${BUILD_DIR}
+cp -r ${DIR}/bin ${BUILD_DIR}
 
 cd ${DIR}/build
 
 coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/nginx-${ARCH}.tar.gz
-coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/mariadb-${ARCH}.tar.gz
-coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/php7-${ARCH}.tar.gz
 coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/python-${ARCH}.tar.gz
-
-mv ${BUILD_DIR}/php7 ${BUILD_DIR}/php
 
 ${BUILD_DIR}/python/bin/pip install -r ${DIR}/requirements.txt
 
 cd ${DIR}/build
-wget https://wordpress.org/wordpress-${WORDPRESS_VERSION}.tar.gz --progress dot:giga
-tar xf wordpress-${WORDPRESS_VERSION}.tar.gz -C ${BUILD_DIR}
-cd ${BUILD_DIR}/wordpress
-
-patch -p0 < ${DIR}/patches/wp-load.patch
+wget https://github.com/tectiv3/standardfile/releases/download/v${STANDARD_FILES_VERSION}/standardfile_${STANDARD_FILES_VERSION}_linux_${STANDARD_FILES_ARCH}.tar.gz --progress dot:giga
+tar xf STANDARD_FILES_ARCH -C ${BUILD_DIR}/bin
 
 cd ${DIR}
 
-cp -r ${DIR}/bin ${BUILD_DIR}
 cp -r ${DIR}/config ${BUILD_DIR}/config.templates
 cp -r ${DIR}/hooks ${BUILD_DIR}
 
-cd ${DIR}/build/
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --progress dot:giga
-sed -i 's/;phar.readonly = On/phar.readonly = Off/g' /etc/php5/cli/php.ini
-php wp-cli.phar --allow-root cli info
-phar extract -f wp-cli.phar -i utils.php phar
-cd phar/vendor/wp-cli/wp-cli/php
-patch -p0 < ${DIR}/patches/wp-cli.patch
-cd ${DIR}/build/
-phar list -f wp-cli.phar -i utils.php
-phar delete -f wp-cli.phar -e vendor/wp-cli/wp-cli/php/utils.php
-phar add -f wp-cli.phar phar 
-phar list -f wp-cli.phar -i utils.php
-
-php wp-cli.phar --allow-root cli info
-cp wp-cli.phar ${BUILD_DIR}/bin/wp-cli.phar
-
-wget https://downloads.wordpress.org/plugin/ldap-login-for-intranet-sites.${WORDPRESS_LDAP_VERSION}.zip --progress dot:giga
-unzip ldap-login-for-intranet-sites.${WORDPRESS_LDAP_VERSION}.zip
-cd ldap-login-for-intranet-sites
-patch -p0 < ${DIR}/patches/ldap.patch
-cd ..
-mv ldap-login-for-intranet-sites ${BUILD_DIR}/wordpress/wp-content/plugins/
-
-mv ${BUILD_DIR}/wordpress/wp-content ${BUILD_DIR}/wp-content.template
-ln -sf /var/snap/wordpress/common/wp-content ${BUILD_DIR}/wordpress/wp-content
 mkdir ${DIR}/build/${NAME}/META
 echo ${NAME} >> ${DIR}/build/${NAME}/META/app
 echo ${VERSION} >> ${DIR}/build/${NAME}/META/version
