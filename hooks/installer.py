acd5a30a4f4b2a08baf3a04be459a37253563162
Boris Rybalkin <ribalkin@gmail.com>
2019-04-04 18:48:10 +0100

    add nginx temp dir

--- hooks/installer.py
+++ hooks/installer.py
@@ -31,7 +31,8 @@ class Installer:
         linux.useradd(USER_NAME, home_folder=home_folder)
         
         fs.makepath(join(self.app_data_dir, 'log'))
-        
+        fs.makepath(join(self.app_data_dir, 'nginx'))
+      
         storage.init_storage(APP_NAME, USER_NAME)
 
         templates_path = join(self.app_dir, 'config.templates')
