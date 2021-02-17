4cfc66b8396fce62f67c28f02a2e0e010a87e258
Boris Rybalkin <ribalkin@gmail.com>
2021-02-17 19:34:25 +0000

    fix download url

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ fi
 SNAP_ARCH=$(dpkg --print-architecture)
 VERSION=$2
 
-DOWNLOAD_URL=http://artifact.syncloud.org/3rdparty
+DOWNLOAD_URL=https://github.com/syncloud/3rdparty/releases/download/1
 
 rm -rf ${DIR}/build
 BUILD_DIR=${DIR}/build/${NAME}
