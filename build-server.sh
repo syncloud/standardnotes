8f457f85b20de17225c59a536ea757f73ffe6266
Boris Rybalkin <ribalkin@gmail.com>
2021-08-28 22:07:42 +0100

    fix server build

--- build-server.sh
+++ build-server.sh
@@ -10,6 +10,6 @@ cd ${DIR}/build
 wget -c https://github.com/mdouchement/standardfile/archive/refs/tags/v${VERSION}.tar.gz --progress dot:giga
 tar xf v${VERSION}.tar.gz
 cd standardfile-${VERSION}
-go build -o ${BUILD_DIR}/bin/standardfile
+go build -ldflags "-s -w -X main.version=${VERSION} -X main.revision=${DRONE_BUILD_NUMBER} -X main.date=$(date +%Y-%m-%d~%H:%M:%S)" -o ${BUILD_DIR}/bin/standardfile cmd/sfc
 mkdir ${BUILD_DIR}/bin
 tar xf standardfile_${STANDARD_FILES_VERSION}_linux_${STANDARD_FILES_ARCH}.tar.gz -C ${BUILD_DIR}/bin
