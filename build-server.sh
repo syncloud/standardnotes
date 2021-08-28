cc43d5ab11d5d1fd15158bb678a8b6207d13d232
Boris Rybalkin <ribalkin@gmail.com>
2021-08-28 22:14:26 +0100

    clean

--- build-server.sh
+++ build-server.sh
@@ -11,5 +11,3 @@ wget -c https://github.com/mdouchement/standardfile/archive/refs/tags/v${VERSION
 tar xf v${VERSION}.tar.gz
 cd standardfile-${VERSION}
 go build -ldflags "-s -w -X main.version=${VERSION} -X main.revision=${DRONE_BUILD_NUMBER} -X main.date=$(date +%Y-%m-%d~%H:%M:%S)" -o ${BUILD_DIR}/bin/standardfile cmd/sfc/main.go
-mkdir ${BUILD_DIR}/bin
-tar xf standardfile_${STANDARD_FILES_VERSION}_linux_${STANDARD_FILES_ARCH}.tar.gz -C ${BUILD_DIR}/bin
