a1744c227112e85be05492a1584067ef284935a9
Boris Rybalkin <ribalkin@gmail.com>
2021-09-19 08:34:41 +0100

    fix docker

--- package-python.sh
+++ package-python.sh
@@ -3,6 +3,9 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
+apt update
+apt -y install libltdl7
+
 BUILD_DIR=${DIR}/build/notes/python
 docker ps -a -q --filter ancestor=python:syncloud --format="{{.ID}}" | xargs docker stop | xargs docker rm || true
 docker rmi python:syncloud || true
