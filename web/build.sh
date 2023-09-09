7236b220fc228b23dc1e2e3f21b882c872d0b9f5
Boris Rybalkin <ribalkin@gmail.com>
2023-09-09 21:17:50 +0100

    fix sync server

--- web/build.sh
+++ web/build.sh
@@ -1,19 +1,15 @@
 #!/bin/sh -e
 
-VERSION=3.172.5
+VERSION=3.172.13
 DIR=$(pwd)
 BUILD_DIR=$DIR/build/snap
 cd ${DIR}/build
 
-apk add --update --no-cache alpine-sdk python3 git yarn tzdata
-git clone https://github.com/standardnotes/web.git src
-cd src
-git checkout @standardnotes/desktop@${VERSION}
-git submodule update --init --force --remote
-. $DIR/web/.env
+apk add --update --no-cache alpine-sdk python3 yarn tzdata
+wget "https://github.com/standardnotes/app/archive/refs/tags/@standardnotes/web@$VERSION.tar.gz" web.tar.gz
+tar xf web.tar.gz
+cd app--standardnotes-web-$VERSION
+sed -i '#https://api.standardnotes.com#/api#' packages/web/src/index.html
 yarn install
 yarn build:web
-
 cp -r packages/web/dist ${BUILD_DIR}/web
-#cp -r public/* ${BUILD_DIR}/web
-#cp -r ${DIR}/web/index.html ${BUILD_DIR}/web
