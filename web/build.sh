47b55705433a5d2af4d53c12634da73048594353
Boris Rybalkin <ribalkin@gmail.com>
2023-09-10 08:06:13 +0100

    fix sed

--- web/build.sh
+++ web/build.sh
@@ -9,7 +9,7 @@ apk add --update --no-cache alpine-sdk python3 yarn tzdata
 wget "https://github.com/standardnotes/app/archive/refs/tags/@standardnotes/web@$VERSION.tar.gz" -O web.tar.gz
 tar xf web.tar.gz
 cd app--standardnotes-web-$VERSION
-sed -i '#https://api.standardnotes.com#/api#' packages/web/src/index.html
+sed -i 's#https://api.standardnotes.com#/api#g' packages/web/src/index.html
 yarn install
 yarn build:web
 cp -r packages/web/dist ${BUILD_DIR}/web
