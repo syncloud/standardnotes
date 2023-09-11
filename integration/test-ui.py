f537f8cdf4e5c20cedf3f03cc014167c9a786dc0
Boris Rybalkin <ribalkin@gmail.com>
2023-09-11 18:25:31 +0100

    test 1

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -77,9 +77,10 @@ def test_login(selenium, ui_mode):
     selenium.find_by(By.XPATH, "//input[@type='password']").send_keys('pass1234')
     selenium.click_by(By.XPATH, "//button[text()='Sign in']")
     selenium.invisible_by(By.XPATH, "//button[text()='Sign in']")
+    selenium.screenshot('test-1')
     selenium.click_by(By.XPATH, "(//footer//button)[1]")
-    selenium.screenshot('test')
     selenium.find_by(By.XPATH, "//div[contains(text(), 'signed in as')]")
+    selenium.screenshot('test-2')
     #selenium.find_by(By.XPATH, "//div[contains(text(), 'signed in as')]")
 
 
