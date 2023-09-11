572a341f81c2c48c5d7b373b4b8021fdc1f143a0
Boris Rybalkin <ribalkin@gmail.com>
2023-09-11 20:23:14 +0100

    upgrade

--- integration/lib.py
+++ integration/lib.py
@@ -1,2 +1,5 @@
+from selenium.webdriver.common.by import By
+
 def login(selenium, device_user, device_password):
+    selenium.find_by(By.XPATH, "//div[contains(.,'Sign in to sync your notes')]")
     selenium.screenshot('upgrade')
