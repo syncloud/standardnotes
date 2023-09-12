02ce58c061dcf705b204c8378c783603d414b4eb
Boris Rybalkin <ribalkin@gmail.com>
2023-09-12 16:21:51 +0100

    fix upgrade

--- integration/test-upgrade.py
+++ integration/test-upgrade.py
@@ -39,6 +39,6 @@ def test_login(selenium, ui_mode):
     selenium.open_app()
     selenium.find_by(By.XPATH, "//div[contains(.,'Sign in to sync your notes')]")
     selenium.screenshot('upgrade-login')
-    selenium.click_by(By.XPATH, "(//footer//button)[1]")
+    #selenium.click_by(By.XPATH, "(//footer//button)[1]")
     lib.login(selenium, ui_mode)
     selenium.screenshot('upgrade')
