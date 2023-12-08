260dd34bd8267cb52b92cf4fc1d940cdef58ac61
Boris Rybalkin <ribalkin@gmail.com>
2023-12-08 13:16:47 +0000

    fix nginx start

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -2,11 +2,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-if [[ -z "$1" ]]; then
-    echo "usage $0 [start|stop]"
-    exit 1
-fi
-
 SOCKET=${SNAP_DATA}/standardnotes.socket
 timeout 100 /bin/bash -c 'until [ -S '${SOCKET}' ]; do echo "waiting for '${SOCKET}'"; sleep 1; done'
 /bin/rm -f ${SNAP_COMMON}/web.socket
