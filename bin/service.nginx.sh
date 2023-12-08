5490a410aa675cce0da7958a806450db33616532
Boris Rybalkin <ribalkin@gmail.com>
2023-12-08 10:11:17 +0000

    clean old logs

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -8,24 +8,7 @@ if [[ -z "$1" ]]; then
 fi
 
 SOCKET=${SNAP_DATA}/standardnotes.socket
-
-case $1 in
-pre-start)
-    timeout 100 /bin/bash -c 'until [ -S '${SOCKET}' ]; do echo "waiting for '${SOCKET}'"; sleep 1; done'
-    /bin/rm -f ${SNAP_COMMON}/web.socket
-    exec ${DIR}/nginx/sbin/nginx -t -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
-    ;;
-start)
-    exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
-    ;;
-reload)
-    ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -s reload -p ${DIR}/nginx
-    ;;
-stop)
-    ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -s stop -p ${DIR}/nginx
-    ;;
-*)
-    echo "not valid command"
-    exit 1
-    ;;
-esac
+timeout 100 /bin/bash -c 'until [ -S '${SOCKET}' ]; do echo "waiting for '${SOCKET}'"; sleep 1; done'
+/bin/rm -f ${SNAP_COMMON}/web.socket
+/bin/rm -f ${SNAP_COMMON}/log/nginx*.log
+exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -e stderr
